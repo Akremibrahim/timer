8f2cd2147713d586f6de892de21cf5bc391f26b8
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2021-12-06 01:02:48 +0000

    completed simple timer task

---
+++ timer1.js
@@ -0,0 +1,27 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+const timer = function() {
+  
+  let inputArray = args;
+  
+  let hasNegative = inputArray.some(num => num < 0);
+  if ((inputArray.length === 0) && (hasNegative)) {
+    return;
+  }
+  
+  inputArray.sort((a,b) => {
+    return a - b;
+  });
+  console.log("sorted:", inputArray);
+    //loops array
+  for (let x = 0; x < inputArray.length; x ++) {
+    console.log(inputArray[x])
+    console.log([x])
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, inputArray[x] * 1000)
+  }
+
+} 
+timer();
